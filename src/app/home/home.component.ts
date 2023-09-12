6dc313ef230da78c86a45c91148a462e1c55021e
alexissolis02 <142378965+alexissolis02@users.noreply.github.com>
2023-09-12 10:22:35 -0600

    Initial commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { ApiRestService } from '../api-rest.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  preguntas = [
+    {id: 1, pregunta: '¿Cuál es la capital de Francia?'},
+    {id: 2, pregunta: '¿Cuál es la capital de España?'},
+    {id: 3, pregunta: '¿Cuál es la capital de Portugal?'},
+    {id: 4, pregunta: '¿Cuál es la capital de Italia?'},
+  ]
+
+  constructor(private api: ApiRestService){}
+
+  // ngOnInit():void {
+  //   this.consulta()
+  // }
+
+  consulta(){
+  //   this.api.getAllPreguntas().subscribe({
+  //     next: datos => {
+  //       console.log(datos)
+  //     },
+  //     error: e => {}
+  //   })
+  }
+}
